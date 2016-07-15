95faa3e99120bf60268c3af6093d9b3aada5e8aa
Faizal Human <faizalhuman@gmail.com>
2016-07-15 19:17:12 +0200

    Finish landing page update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Hello Big World!!"
+    render html: "Hello 15th July 2016"
   end
   
   def goodbye
